bc5f44966fe6d13bd03148ca839007c54c925f05
unknown <colemanliyah@gmail.com>
2024-07-29 18:02:58 -0400

    manipulating LED with user input

--- Python/serial_communication.py
+++ Python/serial_communication.py
@@ -3,6 +3,7 @@ import time
 import ollama
 import requests
 import json
+from struct import *
 
 arduino = serial.Serial(port='COM3', baudrate=9600, timeout=.1) 
 api_url = "http://localhost:11434/api/chat"
@@ -22,7 +23,7 @@ def model(prompt):
     except httpx.ConnectError as e:
         print(f"Connection error: {e}")
 
-def write_read(x: int):
+def write_read(x):
     arduino.write(bytes(x, 'utf-8'))
     data = arduino.readline()
     return data
@@ -30,17 +31,15 @@ def write_read(x: int):
 def main():
     print("check")
 
-    user_input = input("Input text: ")
-    result = model(user_input)
-    print(result)
+    #user_input = input("Input text: ")
+    #result = model(user_input)
+    #print(result)
 
-    #while True:
-        #led_control = input("How do you want to manipulate the LED? ")
-        #result = generate_text(api_url, model_name, result)
 
-        #num = input("Enter 1 to turn on LED, enter 0 to turn off: ")
-        #value = write_read(num)
-        #print(value)
+    while True:
+        led_control = input("How do you want to manipulate the LED? (1 for on 2 for off) ")
+        value = write_read(num)
+        print(value)
 
 if __name__ == "__main__":
     main()
